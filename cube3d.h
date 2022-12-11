b9d1111167584f9769e36cfcd8371dd212863b42
A.YOGUN <yogun@student.42heilbronn.de>
2022-12-11 20:23:36 +0100

    first commit

---
+++ cube3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/11 19:53:12 by yogun             #+#    #+#             */
+/*   Updated: 2022/12/11 20:05:24 by yogun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE3D_H
+# define CUBE3D_H
+
+# define WIDTH 640
+# define HEIGHT 480
+
+# include "libft/libft.h"
+# include "mlx/mlx.h"
+# include <math.h>
+# include <unistd.h>
+# include <stdio.h>
+
+
+#endif
