6ab2ea1a9b18739271882338e30f43305951f3ba
A.YOGUN <yogun@student.42heilbronn.de>
2023-01-16 19:58:15 +0100

    minor update

--- src/ft_keys.c
+++ src/ft_keys.c
@@ -6,12 +6,39 @@
 /*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 20:58:05 by yogun             #+#    #+#             */
-/*   Updated: 2023/01/16 15:14:15 by yogun            ###   ########.fr       */
+/*   Updated: 2023/01/16 19:46:11 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cube3d.h"
 
+/*
+	In this function we wrote the things will happen when we click
+	the 'A' button. Hence, when we click, our player current position is increased 
+	0,2 time player's cos or sin component. We get this value into
+	my and mx. This value is not the exact value of the player's
+	current position. It is player's effective area kind of.
+	This is why we use 0,2 rather than 0,1. 
+	Because it provides us, for instance collect the key 0,1 before
+	the location of the key. So we can collect the key.
+	
+	With get_map_char() we get from the map the char value
+	from the place where the player is located currently and pack
+	it into <val> variable.
+	
+	Then we check if the value is '0' or 'K'. If it is '0' we can
+	move the player. If it is 'K' we can move the player and also
+	we can get the key. So we set the is_key variable to '1' and
+	we set the sprite variable to '0' and we put the map char
+	to '0' with put_map_char() function. This deletes the key
+	sprite from the map and we write '0' to the map. Also we update
+	is_key variable in our db to '1' to show that we have the key.
+
+	Unless the value is '0' we move the player. We increase the
+	player's current position with 0,1 time player's cos or sin
+	component. If the player is moving we increase the step_num
+	variable to show that the player is moving.
+*/
 void	key_a(t_dB *db)
 {
 	int		my;
@@ -36,12 +63,38 @@ void	key_a(t_dB *db)
 	}
 }
 
+/*
+	In this function we wrote the things will happen when we click
+	the 'W' button. Hence, when we click, our player current position is increased 
+	0,2 time player's cos or sin component. We get this value into
+	my and mx. This value is not the exact value of the player's
+	current position. It is player's effective area kind of. This is why we use 0,2 rather
+	than 0,1. Because it provides us, for instance collect the key 0,1 before
+	the location of the key. So we can collect the key.
+	
+	With get_map_char() we get from the map the char value
+	from the place where the player is located currently and pack
+	it into <val> variable.
+	
+	Then we check if the value is '0' or 'K'. If it is '0' we can
+	move the player. If it is 'K' we can move the player and also
+	we can get the key. So we set the is_key variable to '1' and
+	we set the sprite variable to '0' and we put the map char
+	to '0' with put_map_char() function. This deletes the key
+	sprite from the map and we write '0' to the map. Also we update
+	is_key variable in our db to '1' to show that we have the key.
+
+	Unless the value is '0' we move the player. We increase the
+	player's current position with 0,1 time player's cos or sin
+	component. If the player is moving we increase the step_num
+	variable to show that the player is moving.
+*/
 void	key_w(t_dB *db)
 {
 	int		my;
 	int		mx;
 	char	val;
-
+	
 	mx = db->px + (db->pdx * 0.2);
 	my = db->py + (db->pdy * 0.2);
 	val = get_map_char(mx, my, db);
@@ -60,6 +113,32 @@ void	key_w(t_dB *db)
 	}
 }
 
+/*
+	In this function we wrote the things will happen when we click
+	the 'D' button. Hence, when we click, our player current position is increased 
+	0,2 time player's cos or sin component. We get this value into
+	my and mx. This value is not the exact value of the player's
+	current position. It is player's effective area kind of. This is why we use 0,2 rather
+	than 0,1. Because it provides us, for instance collect the key 0,1 before
+	the location of the key. So we can collect the key.
+	
+	With get_map_char() we get from the map the char value
+	from the place where the player is located currently and pack
+	it into <val> variable.
+	
+	Then we check if the value is '0' or 'K'. If it is '0' we can
+	move the player. If it is 'K' we can move the player and also
+	we can get the key. So we set the is_key variable to '1' and
+	we set the sprite variable to '0' and we put the map char
+	to '0' with put_map_char() function. This deletes the key
+	sprite from the map and we write '0' to the map. Also we update
+	is_key variable in our db to '1' to show that we have the key.
+
+	Unless the value is '0' we move the player. We increase the
+	player's current position with 0,1 time player's cos or sin
+	component. If the player is moving we increase the step_num
+	variable to show that the player is moving.
+*/
 void	key_d(t_dB *db)
 {
 	int		my;
@@ -84,6 +163,32 @@ void	key_d(t_dB *db)
 	}
 }
 
+/*
+	In this function we wrote the things will happen when we click
+	the 'S' button. Hence, when we click, our player current position is increased 
+	0,2 time player's cos or sin component. We get this value into
+	my and mx. This value is not the exact value of the player's
+	current position. It is player's effective area kind of. This is why we use 0,2 rather
+	than 0,1. Because it provides us, for instance collect the key 0,1 before
+	the location of the key. So we can collect the key.
+	
+	With get_map_char() we get from the map the char value
+	from the place where the player is located currently and pack
+	it into <val> variable.
+	
+	Then we check if the value is '0' or 'K'. If it is '0' we can
+	move the player. If it is 'K' we can move the player and also
+	we can get the key. So we set the is_key variable to '1' and
+	we set the sprite variable to '0' and we put the map char
+	to '0' with put_map_char() function. This deletes the key
+	sprite from the map and we write '0' to the map. Also we update
+	is_key variable in our db to '1' to show that we have the key.
+
+	Unless the value is '0' we move the player. We increase the
+	player's current position with 0,1 time player's cos or sin
+	component. If the player is moving we increase the step_num
+	variable to show that the player is moving.
+*/
 void	key_s(t_dB *db)
 {
 	int		my;
@@ -110,6 +215,10 @@ void	key_s(t_dB *db)
 
 /*
 	In this function we define the keys that will be hooked.
+	W,A,S,D are the keys for moving the player.
+	E is the key for opening the door.
+	ESC is the key for exiting the game.
+	Left and right arrow keys are for rotating the player.
 */
 int	ft_key_pressed(int keyboard, t_dB *db)
 {
--- src/keys_rotate.c
+++ src/keys_rotate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   keys_rotate.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bderya <bderya@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 14:28:15 by yogun             #+#    #+#             */
-/*   Updated: 2023/01/11 13:54:30 by bderya           ###   ########.fr       */
+/*   Updated: 2023/01/16 19:54:37 by yogun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,47 @@
 /*
 	Rotate point of view to the right direction
 */
-void	right_key(t_dB *a)
+void	right_key(t_dB *db)
 {
-	a->pa -= 5;
-	a->pa = angle_to_360(a->pa);
-	a->pdx = cos(ft_degree_to_radian(a->pa));
-	a->pdy = -1 * sin(ft_degree_to_radian(a->pa));
+	db->pa -= 5;
+	db->pa = angle_to_360(db->pa);
+	db->pdx = cos(ft_degree_to_radian(db->pa));
+	db->pdy = -1 * sin(ft_degree_to_radian(db->pa));
 }
 
 /*
 	Rotate point of view to the left direction
 */
-void	left_key(t_dB *a)
+void	left_key(t_dB *db)
 {
-	a->pa += 5;
-	a->pa = angle_to_360(a->pa);
-	a->pdx = cos(ft_degree_to_radian(a->pa));
-	a->pdy = -1 * sin(ft_degree_to_radian(a->pa));
+	db->pa += 5;
+	db->pa = angle_to_360(db->pa);
+	db->pdx = cos(ft_degree_to_radian(db->pa));
+	db->pdy = -1 * sin(ft_degree_to_radian(db->pa));
 }
 
 /*
 	Door open and close function.
+	
+	In this function we wrote the things will happen when we click
+	the 'E' button. Hence, when we click, our player current position is increased 
+	0,5 time player's cos or sin component. We get this value into
+	my and mx. This value is not the exact value of the player's
+	current position. It is the value of the place where the player
+	will be located after the movement. This provides us, for instance
+	open the door 0,5 before the location of the door. So we can open the door
+	a bit earlier.
+
+	In if condition, we check if the player's effective area is on the door location and
+	if the player has already collected the key. If the player has the key, we open the door and
+	change the door location to '0' in the map. We also change the is_open variable to '1' to
+	indicate that the door is open. We also change the step_num variable to 1 to indicate that
+	step_num counter has been reseted. We use this value to close the door after a certain 
+	amount of steps. Also to save the door location, we use d_x and d_y variables in database.
+	
+	In else if condition, we check if the player's effective area is on the door location and
+	has the key and the door is open: then we close the door and change the door location to 'D'
+	in the map. We also change the is_open variable to '0' to indicate that the door is closed.
 */
 void	key_e(t_dB *db)
 {
@@ -46,11 +66,6 @@ void	key_e(t_dB *db)
 	my = db->py + (db->pdy * 0.5);
 	if (get_map_char(mx, my, db) == 'D' && db->is_key == '1')
 	{
-		// if (db->is_open == '1')
-		// {
-		// 	put_map_char(db->d_x, db->d_y, db, 'D');
-		// 	printf("x\n");
-		// }
 		db->d_x = mx;
 		db->d_y = my;
 		put_map_char(mx, my, db, '0');
